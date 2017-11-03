b24587ae6cdc1aa7961ef043ef79762f76fbf16e
SahajR1 <sahaj.ramachandran@gmail.com>
2017-11-04 02:03:14 +0530

    Added a GetURL function

--- utils/short-url.go
+++ utils/short-url.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"strings"
+	"regexp"
 )
 
 var characters = "23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_"
@@ -10,6 +11,7 @@ var base = len(characters)
 // To make sure the encoded string is at least 5 characters long
 var adjustment = 10000000
 
+// Encodes the ID of the URL in the database to a string that serves as its short code
 func Encode(num int) string {
 	encodedString := ""
 	num += adjustment
@@ -20,6 +22,7 @@ func Encode(num int) string {
 	return encodedString
 }
 
+// Decodes a short code to get the ID of the URL in the database
 func Decode(str string) int {
 	num,i := 0,0
 	for i < len(str) {
@@ -30,6 +33,12 @@ func Decode(str string) int {
 	return num - adjustment
 }
 
+// Returns the complete URL given host and short code
 func GetURL(host, code string) string {
+	urlRegexp := "^(f|ht)tps?://"
+	hasProtocol, _ := regexp.MatchString(urlRegexp, host)
+	if hasProtocol {
+		return host + "/" + code
+	}
 	return "https://" + host + "/" + code
 }
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -8,12 +8,24 @@ import (
 
 func TestEncodeDecode(t *testing.T) {
 	g := Goblin(t)
-	g.Describe("Utils", func() {
+	g.Describe("Utils: Encode & Decode", func() {
 		g.It("Should encode an int ID", func() {
 			g.Assert(Encode(12345)).Equal("3wwsx")
 		})
 		g.It("Should decode a string", func() {
 			g.Assert(Decode("3wwsx")).Equal(12345)
+		})
+	})
+}
+
+func TestGetURL(t *testing.T) {
+	g := Goblin(t)
+	g.Describe("Utils: GetURL", func() {
+		g.It("Should get the correct URL if protocol identifier is absent", func() {
+			g.Assert(GetURL("example.com", "SomeCode")).Equal("https://example.com/SomeCode")
+		})
+		g.It("Should get the correct URL if protocol identifier is present", func() {
+			g.Assert(GetURL("https://example.com", "SomeCode")).Equal("https://example.com/SomeCode")
 		})
 	})
 }
